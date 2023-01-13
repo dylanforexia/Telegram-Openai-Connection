0a75705c0d1843edc6dbb5dfb4ac4a13667729e0
dylanforexia <122587797+dylanforexia@users.noreply.github.com>
2023-01-13 12:54:33 -0600

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -64,17 +64,17 @@ def generate_response(message: str) -> str:
         - str: formated response for the command
     '''
     if message == '/contactme':
-        return 'You can reach out to me here: https://rajkkapadia-portfolio.onrender.com/'
+        return 'You can reach out to me here: @dylanforexia'
     elif message == '/youtube':
-        return 'You can watch my video tutorials here: https://www.youtube.com/channel/UCOT01XvBSj12xQsANtTeAcQ'
+        return 'You can watch my video tutorials here: https://www.youtube.com/'
     elif message == '/github':
-        return 'You can get helpful piece of code here: https://github.com/RajKKapadia'
+        return 'You can get helpful piece of code here: https://github.com/dylanforexia'
     elif message == '/buyacoffee':
-        return 'If you like my work please consider buying me a coffee here: https://www.buymeacoffee.com/rajkkapadia'
+        return 'If you like my work please consider buying me a coffee here'
     elif message == '/help':
         return 'You can ask almost anything here, but do not belive whatever this bot says. :-)'
     elif message == '/start':
-        return 'Hi, this is a chat-bot that uses OpenAI GPT-3, developed by me with love. I will not spam you for sure.'
+        return 'Hi, this is a chat-bot that uses DecentarlChain AI, developed by Blockchain Costa Rica with love. I will not spam you for sure.'
     else:
         result = text_complition(message)
         if result['status'] == 1:
